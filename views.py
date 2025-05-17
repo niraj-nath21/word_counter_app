777bd79a8d438081e6bfd7c43129737d5e557fb4
ItsSlicee <162071662+ItsSlicee@users.noreply.github.com>
2025-05-17 07:29:15 +0545

    used models(without DB) to send data from backend

--- views.py
+++ views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import features
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = features()
+    feature1.id = 1
+    feature1.name = "Word Counter App"
+    feature1.details = "This is a word counter app that counts the number of word in a given text."
+    feature1.text_area = "Enter your text here...."
+    return render(request, 'index.html', {'feature': feature1}) 
 
 def counter(request):
     texts = request.POST['text'] # Here the text is the name of the input field in the HTML form. So renaming the words variable to texts so there is differecnce between the two.
